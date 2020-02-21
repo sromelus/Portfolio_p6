f78719aa64c40fc9500d051a84353338520c35a1
sromelus <46148909+sromelus@users.noreply.github.com>
2020-02-20 23:39:35 -0500

    add home and about route in the server app.js

--- app.js
+++ app.js
@@ -4,7 +4,36 @@ const {projects} = require('./data.json')
 
 const app = express();
 
+app.set("view engine", "pug")
+app.use('/static',express.static('public'));
 
+app.get("/", (req, res) => {
+  res.redirect('/projects')
+})
+
+app.get("/projects", (req, res) => {
+  res.render('index', { projects })
+})
+
+app.get("/projects/1", (req, res) => {
+  res.render('project')
+})
+
+app.get("/about", (req, res) => {
+  res.render('about')
+})
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+})
+
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('error')
+})
 
 app.listen(3000, () => {
   console.log("The application is running on localhost:3000!")
