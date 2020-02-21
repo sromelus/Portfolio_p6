405a529e7f4761e969f328dab1e4e8fc1096cd22
sromelus <46148909+sromelus@users.noreply.github.com>
2020-02-21 13:58:01 -0500

    add data for show pages and up date package.json to use npm start

--- app.js
+++ app.js
@@ -40,11 +40,3 @@ app.use((err, req, res, next) => {
 app.listen(3000, () => {
   console.log("The application is running on localhost:3000!")
 });
-
-
-// An "index" route (/) to render the "Home" page with the locals set to data.projects
-// An "about" route (/about) to render the "About" page
-// Dynamic "project" routes (/project or /projects) based on the id of the project that render
-// a customized version of the Pug project template to show off each project. Which means adding data, or "locals",
-// as an object that contains data to be passed to the Pug template.
-// Finally, start your server. Your app should listen on port 3000, and log a string to the console that says which port the app is listening to.
