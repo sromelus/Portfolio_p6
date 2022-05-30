d607990f68082952d49c131a0ee6bd913c48e903
Shardly Romelus <rshardlypicard89@gmail.com>
2022-05-30 11:22:09 -0400

    add log.txt to track ips

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,31 @@
 const express = require('express');
-const {projects} = require('../data.json')
+const { projects } = require('../data.json');
+const fs = require('fs');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  const date = new Date().toUTCString();
+  const data = `${date} - IP ADDRESS: ${req.ip}`;
+  fs.writeFileSync('log.txt', data);
+  res.redirect('/projects');
+});
 
-router.get("/", (req, res) => {
-  res.redirect('/projects')
-})
+router.get('/projects', (req, res) => {
+  res.render('index', { projects });
+});
 
-router.get("/projects", (req, res) => {
-  res.render('index', { projects })
-})
-
-
-router.get("/projects/:id", (req, res, next) => {
+router.get('/projects/:id', (req, res, next) => {
   //Checking to see if project id is in projects array, if it is render the show page otherwise, render the homepage
-  const project = projects.filter( project => project.id == req.params.id)
-  if (project.length > 0){
-    return res.render('project', { project })
+  const project = projects.filter((project) => project.id == req.params.id);
+  if (project.length > 0) {
+    return res.render('project', { project });
   } else {
     next();
   }
-})
+});
 
-router.get("/about", (req, res) => {
-  res.render('about')
-})
+router.get('/about', (req, res) => {
+  res.render('about');
+});
 
 module.exports = router;
